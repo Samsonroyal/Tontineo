2801dcfd2738bb10bbeb3ed18d0350b36956632b
Misheck Lukhama <misheck.lukhama001@gmail.com>
2024-01-21 18:43:20 +0000

    added pages to the app

---
+++ lib/tontineo_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tontineo_app/tontine_group_creation.dart';
+
+
+
+class TontineoApp extends StatelessWidget {
+  const TontineoApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: TontineGroupCreation(),
+    );
+  }
+}
