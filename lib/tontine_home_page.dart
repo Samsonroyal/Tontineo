e2b69903d7f473ef96c13c36612a88fccce39b50
Were Samson Bruno <werefast2000@gmail.com>
2024-01-23 11:41:24 +0000

    renamed tontine home page file

---
+++ lib/tontine_home_page.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class TontineHomePage extends StatelessWidget {
+  const TontineHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(248, 243, 243, 1),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "lib/assets/images/avatar.png", // Replace with the actual image asset path
+              height: 40.0,
+              width: 40.0,
+            ),
+            Spacer(),
+            SizedBox(width: 8.0),
+            Text(
+              "Hi Kossi",
+              style: TextStyle(fontSize: 20.0),
+            ),
+            Spacer(),
+            Icon(Icons.notifications, color: Colors.green,),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Column like a pop-up
+            Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "You have no Tontine Group",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your button action here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary:
+                          Colors.green, // Set the background color to green
+                    ),
+                    child: Text(
+                      "Create Tontine",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 150.0),
+
+            // Column with image and text
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    "lib/assets/images/tontine_logo.png", // Replace with the actual image asset path
+                    height: 200.0,
+                    width: double.infinity,
+                    fit: BoxFit.contain,
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    "You have not created a group yet",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'DASHBOARD',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.paypal),
+            label: 'CONTRIBUTIONs',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'SETTINGS',
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.green,
+      unselectedItemColor: Colors.black,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+    ),
+    );
+  }
+}
