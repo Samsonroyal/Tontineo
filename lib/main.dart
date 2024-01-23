cad46f001428b85508eee64b83cb150c6153a5be
Were Samson Bruno <werefast2000@gmail.com>
2024-01-23 19:28:23 +0000

    removed const keyword

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:tontineo_mobile_app/tontineo_app.dart';
+import 'package:tontineo_mobile_app/tontine_app.dart';
 
 
 void main() {
-  runApp(const TontineoApp());
+ runApp(TontineoApp());
 }
 
 
