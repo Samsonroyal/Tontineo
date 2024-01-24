e170be5fc4b7fac00e9fd3c922cbdaabb27a6aaa
ma-za-kpe <makpalyy@gmail.com>
2024-01-24 12:59:13 +0000

    add firebase config and auth lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:tontineo_mobile_app/ui/tontine_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(TontineoApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const TontineoApp());
 }
--- lib/ui/tontine_app.dart
+++ lib/ui/tontine_app.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tontineo_mobile_app/ui/home/home/tontine_contribution_page.dart';
-import 'package:tontineo_mobile_app/auth/login/tontine_login_page.dart';
+import 'package:tontineo_mobile_app/ui/auth/login/tontine_login_page.dart';
 import 'package:tontineo_mobile_app/ui/splash/splash_screen.dart';
 import 'package:tontineo_mobile_app/ui/home/tontine_home_page.dart';
 import 'package:tontineo_mobile_app/ui/home/settings/tontine_settings_page.dart';
-import 'package:tontineo_mobile_app/auth/register/tontineo_signup_page.dart';
+import 'package:tontineo_mobile_app/ui/auth/register/tontineo_signup_page.dart';
 
 class TontineoApp extends StatelessWidget {
   const TontineoApp({super.key});
