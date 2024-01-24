926ebce2780432db545b9164aa67541543a84c27
ma-za-kpe <makpalyy@gmail.com>
2024-01-24 11:22:30 +0000

    refactor folder structure

--- lib/ui/tontine_app.dart
+++ lib/ui/tontine_app.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:tontineo_mobile_app/tontine_contribution_page.dart';
-import 'package:tontineo_mobile_app/tontine_login_page.dart';
-import 'package:tontineo_mobile_app/splash_screen.dart';
-import 'package:tontineo_mobile_app/tontine_home_page.dart';
-import 'package:tontineo_mobile_app/tontine_settings_page.dart';
-import 'package:tontineo_mobile_app/tontineo_signup_page.dart';
-
-
+import 'package:tontineo_mobile_app/ui/home/home/tontine_contribution_page.dart';
+import 'package:tontineo_mobile_app/auth/login/tontine_login_page.dart';
+import 'package:tontineo_mobile_app/ui/splash/splash_screen.dart';
+import 'package:tontineo_mobile_app/ui/home/tontine_home_page.dart';
+import 'package:tontineo_mobile_app/ui/home/settings/tontine_settings_page.dart';
+import 'package:tontineo_mobile_app/auth/register/tontineo_signup_page.dart';
 
 class TontineoApp extends StatelessWidget {
   const TontineoApp({super.key});
 
-@override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tontineo App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-
       initialRoute: '/splash',
-      routes: <String, WidgetBuilder> {
+      routes: <String, WidgetBuilder>{
         '/splash': (context) => const SplashScreen(),
         '/welcome': (context) => const SignUpPage(),
         '/login': (context) => const LoginPage(),
@@ -31,11 +27,7 @@ class TontineoApp extends StatelessWidget {
         '/settings': (context) => const SettingsPage(),
         '/contributions': (context) => const TontineContributions(),
       },
-
       home: const TontineHomePage(),
-
     );
   }
 }
-
-
