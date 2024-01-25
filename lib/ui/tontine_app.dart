4307d87c8077ef64235deda73d7b1dea8fe95980
Were Samson Bruno <werefast2000@gmail.com>
2024-01-25 22:53:20 +0000

    removed unused imports from app

--- lib/ui/tontine_app.dart
+++ lib/ui/tontine_app.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tontineo_mobile_app/ui/home/home/invite_members_page.dart';
-import 'package:tontineo_mobile_app/ui/home/home/tontine_contribution_page.dart';
-import 'package:tontineo_mobile_app/auth/login/tontine_login_page.dart';
-import 'package:tontineo_mobile_app/ui/splash/splash_screen.dart';
+import 'package:tontineo_mobile_app/ui/home/home/contributions/tontine_contribution_page.dart';
 import 'package:tontineo_mobile_app/ui/auth/authentication_page.dart';
 import 'package:tontineo_mobile_app/ui/auth/login/log_in.dart';
 import 'package:tontineo_mobile_app/ui/auth/register/sign_up.dart';
-import 'package:tontineo_mobile_app/ui/home/home/contributions/tontine_contribution_page.dart';
 
 import 'package:tontineo_mobile_app/ui/home/tontine_home_page.dart';
 import 'package:tontineo_mobile_app/ui/home/settings/tontine_settings_page.dart';
@@ -32,10 +29,8 @@ class TontineoApp extends StatelessWidget {
         '/settings': (context) => const SettingsPage(),
 
         '/invite_members': (context) => InviteMembersPage(),
-        '/contributions': (context) => const TontineContributions(),
-
         '/contributions': (context) => TontineContributions(),
-
+      
       },
       home: const AuthenticationFlowScreen(),
     );
