aa49be6f5f93c2a01ed7de331a68e8cb9885f310
Were Samson Bruno <werefast2000@gmail.com>
2024-01-28 01:25:24 +0000

    import firestore

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tontineo_mobile_app/state/auth/authentication_bloc.dart';
 import 'package:tontineo_mobile_app/ui/tontine_app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 void main() async {
