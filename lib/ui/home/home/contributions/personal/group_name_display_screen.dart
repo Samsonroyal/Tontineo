d8336f0b88371e846c0b04b55e26afb522864475
Were Samson Bruno <werefast2000@gmail.com>
2024-01-29 15:31:34 +0000

    created group information class

--- lib/ui/home/home/contributions/personal/group_name_display_screen.dart
+++ lib/ui/home/home/contributions/personal/group_name_display_screen.dart
@@ -1,49 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:tontineo_mobile_app/ui/home/home/invite_members_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class GroupNameDisplayScreen extends StatelessWidget {
+class GroupInformation extends StatefulWidget {
   // Declare a variable to hold the group name
   final String groupName;
 
   // Constructor to receive the group name when navigating to this screen
-  const GroupNameDisplayScreen({Key? key, this.groupName = ''})
+  const GroupInformation({Key? key, this.groupName = ''})
       : super(key: key);
 
   @override
+  _GroupInformationState createState() => _GroupInformationState();
+}
+
+class _GroupInformationState extends State<GroupInformation> {
+  final Stream<QuerySnapshot> _groupStream =
+      FirebaseFirestore.instance.collection('tontines').snapshots();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('You have successfully created a Tontine Group'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Group Name: $groupName',
-              style: const TextStyle(fontSize: 20),
-            ),
-            // Create Tontine Group button
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => InviteMembersPage(),
+    return StreamBuilder<QuerySnapshot>(
+      stream: _groupStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Created Groups'),
+           
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                
+                Expanded(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data =
+                          document.data()! as Map<String, dynamic>;
+                      return ListTile(
+                        title: Text(data['group_name'] ?? 'Default Group Name'),
+                        subtitle: Text((data['group_members'] as List<dynamic>? ?? []).join(', ')),
+                      );
+                    }).toList(),
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // Set the background color to green
-              ),
-              child: const Text(
-                'Click here to add members',
-                style: TextStyle(color: Colors.white),
-              ),
+                ),              
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
