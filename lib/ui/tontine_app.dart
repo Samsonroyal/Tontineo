cd2027819f181c6717e30deea77ccceb20afbbd8
Were Samson Bruno <werefast2000@gmail.com>
2024-01-29 22:46:42 +0000

    delete unused imports

--- lib/ui/tontine_app.dart
+++ lib/ui/tontine_app.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:tontineo_mobile_app/data/model/user.dart';
-import 'package:tontineo_mobile_app/ui/home/home/invite_members_page.dart';
-import 'package:tontineo_mobile_app/ui/home/home/tontine_contribution_page.dart';
-import 'package:tontineo_mobile_app/auth/login/tontine_login_page.dart';
-import 'package:tontineo_mobile_app/ui/splash/splash_screen.dart';
-import 'package:tontineo_mobile_app/ui/auth/authentication_page.dart';
 import 'package:tontineo_mobile_app/ui/auth/login/log_in.dart';
 import 'package:tontineo_mobile_app/ui/auth/register/sign_up.dart';
-
-import 'package:tontineo_mobile_app/ui/home/home/tontine_group_creation.dart';
+import 'package:tontineo_mobile_app/ui/home/home/contributions/personal/make_contributions_page.dart';
+import 'package:tontineo_mobile_app/ui/home/home/invite_members_page.dart';
 import 'package:tontineo_mobile_app/ui/home/home/tontines/dashboard_page.dart';
-
-import 'package:tontineo_mobile_app/ui/home/tontine_home_page.dart';
 import 'package:tontineo_mobile_app/ui/home/settings/tontine_settings_page.dart';
+import 'package:tontineo_mobile_app/ui/home/tontine_home_page.dart';
 
 class TontineoApp extends StatefulWidget {
   const TontineoApp({Key? key}) : super(key: key);
@@ -22,8 +15,6 @@ class TontineoApp extends StatefulWidget {
 }
 
 class _TontineoAppState extends State<TontineoApp> {
-
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,15 +28,11 @@ class _TontineoAppState extends State<TontineoApp> {
       routes: <String, WidgetBuilder>{
         '/signup': (context) => const SignupScreen(),
         '/login': (context) => const LoginScreen(),
-        '/dashboard': (context) => const DashboardPage(),        
-        '/home': (context) => TontineHomePage(),
+        '/dashboard': (context) => const DashboardPage(),
+        '/home': (context) => const TontineHomePage(),
         '/settings': (context) => const SettingsPage(),
-
         '/invite_members': (context) => InviteMembersPage(),
-        '/contributions': (context) => const TontineContributions(),
-
-        '/contributions': (context) => TontineContributions(),
-
+        '/contributions': (context) => MakeContributionsPage(),
       },
     );
   }
