4be7f5d8793c7343e23d35bfc2a9ac7da7653640
Were Samson Bruno <werefast2000@gmail.com>
2024-01-29 01:03:28 +0000

    added dashboard screen

--- lib/ui/home/home/tontines/created_tontines.dart
+++ lib/ui/home/home/tontines/created_tontines.dart
@@ -1,73 +1,120 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Dashboard extends StatelessWidget {
+final Stream<QuerySnapshot> tontineSnapshots =
+    FirebaseFirestore.instance.collection('tontineCollections').snapshots();
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({Key? key}) : super(key: key);
+
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Column(
-        children: [
-          TabBar(
-            tabs: [
-              Tab(text: 'Tontines'),
-              Tab(text: 'Members'),
-              Tab(text: 'Contributions'),
-            ],
-          ),
-          Card(
-            child: Column(
-              children: [
-                Text('Tontines Data'),
-                Text('Members:'),
-                Text('Name: Were Samson'),
-                Text('Phone Number: 0786871705'),
-                Text('Name: Lion Heart'),
-              ],
+        title: const Text('Dashboard'),
+        bottom: TabBar(
+          controller: _tabController,
+          physics: ScrollPhysics(),
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(Icons.bookmark),
+              text: 'Tontines',
             ),
-          ),
-          SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            Tab(
+              icon: Icon(Icons.group),
+              text: 'Members',
+            ),
+            Tab(
+              icon: Icon(Icons.money_rounded),
+              text: 'Contributions',
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6.0),
-                      ),
-                      padding: const EdgeInsets.all(8.0),
-                      minimumSize: const Size.fromHeight(70.0),
-                    ),                
-                child: Text('Invite members',style: TextStyle(color: Colors.white),
-                )
-                ,
-                onPressed: () {
-                  // TODO: Implement invite members functionality
-                },
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: <Widget>[
+                    Center(
+                      child: Text("Tontines"),
+                    ),
+                    Center(
+                      child: Text("Members"),
+                    ),
+                    Center(
+                      child: Text("Contributions"),
+                    ),
+                  ],
+                ),
               ),
-              OutlinedButton(
-                style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(6.0),
                       ),
                       padding: const EdgeInsets.all(8.0),
                       minimumSize: const Size.fromHeight(70.0),
-                    ),      
-                
-                child: Text('Notify members', style: TextStyle(color: Colors.green)),
-                onPressed: () {
-                  // TODO: Implement notify members functionality
-                },
+                    ),
+                    child: Text(
+                      'Invite members',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      // TODO: Implement invite members functionality
+                    },
+                  ),
+                  OutlinedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      minimumSize: const Size.fromHeight(70.0),
+                    ),
+                    child: Text('Notify members',
+                        style: TextStyle(color: Colors.green)),
+                    onPressed: () {
+                      // TODO: Implement notify members functionality
+                    },
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
+        ),
+      ),      
     );
   }
 }
+
