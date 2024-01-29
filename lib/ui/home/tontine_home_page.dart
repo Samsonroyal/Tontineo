623de2242e706a8d4cb221debd5f46e1e8eed7cb
Were Samson Bruno <werefast2000@gmail.com>
2024-01-29 13:12:23 +0000

    import firebase auth for userstate

--- lib/ui/home/tontine_home_page.dart
+++ lib/ui/home/tontine_home_page.dart
@@ -1,26 +1,32 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tontineo_mobile_app/data/model/AdminModel.dart';
-import 'package:tontineo_mobile_app/data/model/user.dart';
 import 'package:tontineo_mobile_app/ui/home/home/contributions/tontine_contribution_page.dart';
 import 'package:tontineo_mobile_app/ui/home/home/tontine_group_creation.dart';
 import 'package:tontineo_mobile_app/ui/home/home/tontines/created_tontines.dart';
 import 'package:tontineo_mobile_app/ui/home/settings/tontine_settings_page.dart';
 
-
 class TontineHomePage extends StatefulWidget {
-  final UserModel user;
-  const TontineHomePage({super.key, required this.user});
+  const TontineHomePage({super.key});
 
   @override
   _TontineHomePageState createState() => _TontineHomePageState();
 }
 
 class _TontineHomePageState extends State<TontineHomePage> {
+  AdminModel? user;
+
+  @override
+  void initState() {
+    user = null;
+    getUser();
+    super.initState();
+  }
+
   int activeIndex = 0;
   List<Widget> pages = [
     const TontineGroupCreation(),
-    const DashboardPage(),    
+    const DashboardPage(),
     RecordContributions(),
     const SettingsPage(),
   ];
@@ -30,10 +36,9 @@ class _TontineHomePageState extends State<TontineHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldState,
-      
       bottomNavigationBar: _myBottomNav(),
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 144, 144, 144),
         elevation: 5,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -45,8 +50,8 @@ class _TontineHomePageState extends State<TontineHomePage> {
             ),
             Spacer(),
             const SizedBox(width: 8.0),
-            const Text(
-              "Hi",
+            Text(
+              user != null ? "Hi, ${user?.email}" : "Hi, User",
               style: TextStyle(fontSize: 20.0),
             ),
             Spacer(),
@@ -57,7 +62,7 @@ class _TontineHomePageState extends State<TontineHomePage> {
           ],
         ),
       ),
-      body: pages[activeIndex],     
+      body: pages[activeIndex],
     );
   }
 
@@ -73,7 +78,7 @@ class _TontineHomePageState extends State<TontineHomePage> {
       ],
       type: BottomNavigationBarType.fixed,
       elevation: 7,
-      selectedItemColor: const Color.fromARGB(255, 33, 243, 61),
+      selectedItemColor: Color.fromARGB(255, 35, 199, 57),
       unselectedItemColor: const Color.fromARGB(255, 123, 118, 118),
       showSelectedLabels: true,
       showUnselectedLabels: true,
@@ -86,32 +91,21 @@ class _TontineHomePageState extends State<TontineHomePage> {
     );
   }
 
-  Future<AdminModel?> getUser(String userId) async {
+  getUser() {
     // Reference to the 'users' collection
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
-
-    // Get the document snapshot corresponding to the provided user ID
-    DocumentSnapshot documentSnapshot = await users.doc(userId).get();
-    debugPrint("userrr 2 ..... $documentSnapshot");
-    // Check if the document exists
-    if (documentSnapshot.exists) {
-      // Extract data from the document snapshot
-      Map<String, dynamic> data =
-          documentSnapshot.data() as Map<String, dynamic>;
-
-      // Create an AdminModel object from the data
-      AdminModel user = AdminModel(
-        name: data['name'],
-        email: data['email'],
-        uid: data['uid'],
-        phone: data['phone'],
-        selectedUserType: data['selectedUserType'],
-      );
-      debugPrint("user ..... $user");
-      return user;
-    } else {
-      debugPrint('Document does not exist');
-      return null;
-    }
+    FirebaseAuth.instance.authStateChanges().listen((event) {
+      if (event == null) {
+        return;
+      }
+      print("hello" + event.toString());
+      setState(() {
+        user = AdminModel(
+          name: event.email!,
+          email: event.email!,
+          uid: event.uid,
+          phone: event.phoneNumber ?? '',
+        );
+      });
+    });
   }
 }
